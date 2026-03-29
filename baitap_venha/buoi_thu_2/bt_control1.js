aa98fc3805e67a03394a11635f46b15f56abf253
anhdo2024 <anhdn311082@gmail.com>
2026-03-30 02:36:26 +0700

    remove files/folders

--- baitap_venha/buoi_thu_2/bt_control1.js
+++
@@ -1,8 +0,0 @@
-let actualResult = "Login Success"
-let expectedResult = "Login Success"
-
-if (actualResult === expectedResult) {
-    console.log("Passed");
-} else {
-    console.log("Failed");
-}
--- baitap_venha/buoi_thu_2/bt_control3.js
+++
@@ -1,8 +0,0 @@
-let totalBill = 600;
-let ismember = true;
-
-if (totalBill > 500 && ismember === true) {
-    console.log("Bạn được giảm giá 20%");
-} else {
-    console.log("Bạn không được giảm giá");
-}
