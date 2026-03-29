36aac6b56bdadd31ca67f2d4a5d72a3f15c0319f
anhdo2024 <anhdn311082@gmail.com>
2026-03-30 02:29:13 +0700

    fix submodule issue

---
+++ baitap_venha/buoi_thu_2/bt_control1.js
@@ -0,0 +1,8 @@
+let actualResult = "Login Success"
+let expectedResult = "Login Success"
+
+if (actualResult === expectedResult) {
+    console.log("Passed");
+} else {
+    console.log("Failed");
+}
---
+++ baitap_venha/buoi_thu_2/bt_control3.js
@@ -0,0 +1,8 @@
+let totalBill = 600;
+let ismember = true;
+
+if (totalBill > 500 && ismember === true) {
+    console.log("Bạn được giảm giá 20%");
+} else {
+    console.log("Bạn không được giảm giá");
+}
